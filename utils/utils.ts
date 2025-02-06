1f9b5b7f71d15a9824cc8e00e5068fafbed5e1f9
Andrew McGuier <andrew@echogatetech.com>
2025-02-06 10:11:13 -0500

    Matching complete

--- utils/utils.ts
+++ utils/utils.ts
@@ -4,7 +4,7 @@ import { Json } from "./supabase/schema";
 import { table } from "console";
 
 /**
- * Redirects to a specified path with an encoded message as a query parameter.
+n * Redirects to a specified path with an encoded message as a query parameter.
  * @param {('error' | 'success')} type - The type of message, either 'error' or 'success'.
  * @param {string} path - The path to redirect to.
  * @param {string} message - The message to be encoded and added as a query parameter.
@@ -18,67 +18,34 @@ export function encodedRedirect(
     return redirect(`${path}?${type}=${encodeURIComponent(message)}`);
 }
 
-// *************
-
-// separate the array into two by ticket type (or maybe just filter readyAttendees twice?). idk, something like...
-
-// maleAttendees
-// femaleAttendees
-
-// "Basic" algorithm: one group stays seated, the other group moves down. Doesn't work here because people might bow out between rounds, for one thing. So not that.
-
-// This falls under the combinatorics bucket of an "assignment problem". Basically, we want to associate every possible pair with a "cost". This can default to 0, and be 1 iff the pair has already met (so we don't rematch).
-// If we make an M-by-F matrix, where each row corresponds to a maleAttendee and each column a femaleAttendee, we can process it based on something called the "Hungarian algorithm"
-// https://en.wikipedia.org/wiki/Hungarian_algorithm#Matrix_interpretation
-// To keep this from being deterministic, we can randomly order both sets of attendees before making the matrix and doing the algorithm.
-
-// Steps:
-// - 0. Make the matrix square by adding either rows or columns of 0s ("ghost dates" of the less common gender)
-// - 1. Subtract the smallest element of each row from every element of that row.
-// - 2. Subtract the smallest element of each column from every element of that column.
-// - 3. Check for an optimal assignment,,, (there's more)
-
-// *********
-
-const MAX_ITERATIONS = 100;
-
-const getRandom = <T = unknown>(list: T[]) =>
-    list[Math.floor(Math.random() * list.length)];
-
-const getMatch = (
-    attendeeId: string,
-    pastMatches: Record<string, string[]>,
-    potentialMatchIds: string[]
-) => {
-    const blacklist = [attendeeId, ...(pastMatches[attendeeId] || [])];
-
-    let iterations = 0;
-    let match = null;
-
-    while (!match && iterations < MAX_ITERATIONS) {
-        const randomAttendee = getRandom(potentialMatchIds);
-        if (!blacklist.includes(randomAttendee)) match = randomAttendee;
-        iterations++;
-    }
-    return match;
-};
 
 export const generatePossibleMatches = (readyAttendeesList: {
     id: string;
     name: string;
     ticketAs: string | "Women" | "Men";
 }[], previousMatches: Record<string, string[]>, paritionByTicket: boolean) => {
-
+    // we put the ids in lexographical order, since the order is irrelevant this lets us
+    // avoid duplicates
     const sorted = readyAttendeesList.toSorted((a, b) => a.id == b.id ? 0 : a.id < b.id ? -1 : 1);
-    const potentialMatches = []
+
+    const potentialMatches: Record<string, string>[] = []
+
     for (let i = 0; i < sorted.length; i++) {
         const attendee = sorted[i];
         const alreadyMatched = previousMatches[attendee.id];
+
+        // since we ordered the list we only have to check for matches in front of our position
         for (let j = i + 1; j < sorted.length; j++) {
+
             const potentialMatch = sorted[j];
+
+            // we don't want any matches with people we already matched with
             if (!alreadyMatched.includes(potentialMatch.id)) {
+
+                // if we're not partioning by tickettype, we're good to go
+                // otherwise ensure we have a different ticket type before we match,
                 if (!paritionByTicket || attendee.ticketAs !== potentialMatch.ticketAs) {
-                    potentialMatches.push({ 1: attendee.id, 2: potentialMatch.id });
+                    potentialMatches.push({ [attendee.id]: potentialMatch.id });
                 }
             }
         }
@@ -86,12 +53,70 @@ export const generatePossibleMatches = (readyAttendeesList: {
     return potentialMatches;
 }
 
-export const countPotentialMatchesPerAtendee = (matches: Record<string, string>[]) => {
+
+
+export const chooseOptimalMatches = (potentialMatches: Record<string, string>[], potentialMatchCounts: Record<string, number>, skippedLastRound: string[]) => {
+    // note this is a heuristic
+    // I went down the rabbit hole on this we'd actually need a graph search algorithm like
+    // the blossom algorithm to find the optimal solution,
+
+    // general approach, sort the available users by the number of potential matches they have
+    // first we pair up everyone who was skipped last round
+    // next we pair the rest up heuristically matching the most constrained people together.
+    // all pairing match the person to their neighbor with the fewest potential matches
+
+
+    const sortMatchesByCount = (a: string, b: string) => potentialMatchCounts[b] - potentialMatchCounts[a];
+    let sortedIds: string[] = Object.keys(potentialMatchCounts).toSorted(sortMatchesByCount);
+
+
+    let sortedPreviousSkips: string[] = skippedLastRound.toSorted(sortMatchesByCount);
+
+    const skipped: string[] = [];
+    const matches: Record<string, string>[] = [];
+
+
+    const tryFindMatch = (uid: string) => {
+        const maybeMatches = potentialMatches
+            .filter(m => Object.keys(m).includes(uid) || Object.values(m).includes(uid))
+            .map(m => Object.keys(m).includes(uid) ? m[uid] : Object.keys(m)[0])
+            .filter(m => sortedIds.includes(m))
+            .toSorted(sortMatchesByCount);
+        if (maybeMatches.length > 0) {
+            const thisMatch = maybeMatches.pop() ?? "";
+            matches.push({ [uid]: thisMatch });
+            sortedIds = sortedIds.filter(id => id !== uid && id !== thisMatch);
+        } else {
+            sortedIds = sortedIds.filter(id => id !== uid);
+            skipped.push(uid)
+        }
+
+    };
+
+
+    while (sortedPreviousSkips.length > 0) {
+        const uid: string = sortedPreviousSkips.pop() ?? ""; // can't happen
+        tryFindMatch(uid);
+    }
+
+    while (sortedIds.length > 0) {
+        const uid: string = sortedIds.pop() ?? ""; // can't happen
+        tryFindMatch(uid);
+    }
+
+    return { matches, skipped };
+
+
+};
+
+export const countPotentialMatchesPerAttendee = (matches: Record<string, string>[]) => {
     const counts: Record<string, number> = {};
     // initialize
     matches.forEach(match => {
-        counts[match[1]] = match[1] in counts ? counts[match[1]] + 1 : 1;
-        counts[match[2]] = match[2] in counts ? counts[match[2]] + 1 : 1;
+        const ky = Object.keys(match)[0] ?? "";
+        const vl = match[ky];
+        counts[ky] = ky in counts ? counts[ky] + 1 : 1;
+        counts[vl] = vl in counts ? counts[vl] + 1 : 1;
     });
     return counts;
 };
@@ -109,86 +134,57 @@ export const generateMatches = (
     table_size: number,
     event_type?: string
 ) => {
-    // Split the list into two
-    let smallerSet = readyAttendeesList
-        .filter((attendee) => attendee.ticketAs === "Men")
-        .map(({ id }) => id);
-    let largerSet = readyAttendeesList
-        .filter((attendee) => attendee.ticketAs === "Women")
-        .map(({ id }) => id);
 
-    if (smallerSet.length > largerSet.length) {
-        [smallerSet, largerSet] = [largerSet, smallerSet];
-    }
+    // note, set to false to do non-dating app style
+    const potentialMatches = generatePossibleMatches(readyAttendeesList, previousMatches, true);
 
-    // // Randomize the arrays
-    // let shuffledSmallerSet = smallerSet
-    //   .map((value) => ({ value, sort: Math.random() }))
-    //   .sort((a, b) => a.sort - b.sort)
-    //   .map(({ value }) => value);
+    const potentialMatchCounts = countPotentialMatchesPerAttendee(potentialMatches);
 
-    // let shuffledLargerSet = largerSet
-    //   .map((value) => ({ value, sort: Math.random() }))
-    //   .sort((a, b) => a.sort - b.sort)
-    //   .map(({ value }) => value);
+    const { matches } = chooseOptimalMatches(potentialMatches, potentialMatchCounts, skippedAttendees);
 
-    // Pair them off and return the matches
-
-    let matchInfoArray = [];
+    let matchInfoArray: {
+        event_round_id: string, attendee_id: string, location: string, match_info: {
+            id: string;
+            name: string;
+            ticketAs: string | "Women" | "Men";
+        } | undefined
+    }[]
+        = [];
     let seats = 0;
     let newMatchList = {} as Record<string, string[]>;
     let noMatchList = [] as string[];
     const roundedTableCount = table_size % 2 !== 0 ? table_size - 1 : table_size;
-    const weightedAttendeeList = [...skippedAttendees, ...smallerSet];
 
-    console.log({ weightedAttendeeList });
-    console.log({ smallerSet }, { skippedAttendees });
-    let matchId: string | null = null;
-    for (const attendeeId of weightedAttendeeList) {
-        if (skippedAttendees.includes(attendeeId)) {
-            matchId = getMatch(attendeeId, previousMatches, smallerSet);
-        } else {
-            matchId = getMatch(attendeeId, previousMatches, largerSet);
-        }
-        console.log("Match ID: ", matchId);
-        if (!matchId) {
-            continue;
-        }
 
+
+    matches.forEach((match) => {
         const table_id = Math.floor(seats / roundedTableCount);
         if (table_id > table_count) {
-            break;
+            return;
         }
         const table = `Table ${String.fromCharCode(table_id + 65)}`;
         seats += 2;
-
+        const attendeeId = Object.keys(match)[0] ?? "";
         matchInfoArray.push({
             event_round_id: round_id,
             attendee_id: attendeeId,
             location: table,
-            match_info: readyAttendeesList.find(({ id }) => id === matchId) as Json,
-        });
-
-        matchInfoArray.push({
-            event_round_id: round_id,
-            attendee_id: matchId,
-            location: table,
-            match_info: readyAttendeesList.find(
-                ({ id }) => id === attendeeId
-            ) as Json,
+            match_info: readyAttendeesList.find(({ id }) => id === match[attendeeId] as Json,
+            )
         });
 
         if (newMatchList[attendeeId]) {
-            newMatchList[attendeeId].push(matchId);
+            newMatchList[attendeeId].push(match[attendeeId]);
         } else {
-            newMatchList[attendeeId] = [matchId];
+            newMatchList[attendeeId] = [match[attendeeId]];
         }
-        if (newMatchList[matchId]) {
-            newMatchList[matchId].push(attendeeId);
+        if (newMatchList[match[attendeeId]]) {
+            newMatchList[match[attendeeId]].push(attendeeId);
         } else {
-            newMatchList[matchId] = [attendeeId];
+            newMatchList[match[attendeeId]] = [attendeeId];
         }
-    }
+    })
+
 
     readyAttendeesList.map((attendee) => {
         if (!matchInfoArray.some((match) => match.attendee_id == attendee.id)) {
